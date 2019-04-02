ae2ead9a7f182d4fb0832d423e03f6a2c5666abc
Estefanía Vizcarra <estefaniavizcarra@MacBook-Air-de-Estefania-2.local>
2019-04-02 01:57:51 -0600

    falta checar resultado

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,71 @@
 $(document).ready(function(){
-    
-    $("#start-button").on("click", stopwatch.start);
-    $(".choice").on("click", stopwatch.correctCount);
-    $("#done-button").on("click", stopwatch.stop);
-   $("#restart-button").on("click", stopwatch.reset);
     $("section").hide();
-    $(".jumbotronfooter").hide();
-});
+    $("#start-button").click(function(){
+        var number = 50;
+        var correctAnswers = 0;
+      $("#start-button").on("click", start);  
+      $("#done-button").on("click", finish);  
+      $("#restart").on("click", restart);  
 
-var intervalId;
-var clockRunning = false;
-var correctAnswers = 0;
-var stopwatch = {
-time= 60,
-correctCount= function() {  
-     if (clockRunning) {
-var selection = $(this).val().trim();
+      function verify() {
 
-            if (selection === "correct" && correctAnswers < 6) {
-                correctAnswers++
-            }
+    var selection = $(this).val().trim();
 
-            else if (correctAnswers > 6) {
-                stopwatch.stop();
-            }
-                else if (!clockRunning) {
-            event.preventDefault();
-                }
-            }
-    reset=function() {
-        stopwatch.stop();
-        stopwatch.time = 60;
-        correctAnswers = 0;
-        $("#time-remaining").text("1:00");
-        $("input[type='radio']").prop('checked', false);
+        if (selection === "correct" && correctAnswers < 6) {
+            correctAnswers++
+        }
+        else if (correctAnswers > 6) {
+            counter.stop();
+         
+        }
     }
-if(start = function() {
-        if (!clockRunning) {
-            intervalId = setInterval(stopwatch.count, 1000);
-            clockRunning = true;
-            $("section").show();
-            $(".card").hide();
-            
+        ;
+      function start(){
+          counter = setInterval(timer, 1000);
+          showMe("section");
+          hideMe(".card");
+          hideMe(".jumbotronfooter")
 
+      }
+      function timer(){
+        number-- 
+        $("#time-remaining").html(  number );
+        if (number === 0){
+          stop(); 
         }
-    },
+      }
+      function stop(){
+          clearInterval(counter); 
+        $(".card").show();
+          $("section").hide();
+          (".jumbotronfooter").show();
+            $("#puntos").html(correctAnswers + "/"   + "6")                
 
-    stop=function() {
-        clearInterval(intervalId);
-        clockRunning = false;
-        $("section").hide();
-        $(".jumbotronfooter").show();
-        $("#puntos").html("Score:" + correctAnswers + "/6");
-    },
 
-    count= function() {
+      }
+      function finish(){
+          number = 1; 
+          clearInterval(counter); 
+          timer();
+          $("#puntos").html(correctAnswers + "/"   + "6")                
+          (".jumbotronfooter").show();
+          $("input[type='radio']").prop('checked', false);
 
-        if (stopwatch.time > 0) {
-            stopwatch.time--;
-            var converted = stopwatch.timeConverter(stopwatch.time);
 
-            $("#puntos").text(converted);
-        }
+      }
+  
+      function restart(){
 
-        //when the clock reaches 0, stop counting down
-        else {
-            stopwatch.stop();
-        }
-
-    },
-
-    timeConverter=function(t) {
-        var minutes = Math.floor(t / 60);
-        var seconds = t - (minutes * 60);
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
-        if (minutes === 0) {
-            minutes = "00";
-        }
-        else if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
-        return minutes + ":" + seconds;
-    }
-    
+          number = 50;
+          start();
+      }
+  
+      function hideMe(e) {
+          $(e).hide();
+      }
+      function showMe(e) {
+          $(e).show();
+      }
+      start(); 
+    });
+  });
